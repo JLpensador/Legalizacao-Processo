a823ab802c93d102469b3f675f3ba51e2d00f196
João Luiz B. Morais <127153172+JLpensador@users.noreply.github.com>
2024-10-14 17:28:20 -0300

    Feat: Adiciona análise de projeto Vercel ao Projeto

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { inject as injectAnalytics } from '@vercel/analytics';
 
 import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule, {
-  ngZoneEventCoalescing: true
-})
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule, {
+    ngZoneEventCoalescing: true,
+  })
+  .catch((err) => console.error(err));
+
+injectAnalytics();
