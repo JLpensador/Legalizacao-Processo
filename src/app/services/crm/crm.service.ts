616ca35936a05cd796cb1033c49bbfb1b64d7d25
João Luiz B. Morais <127153172+JLpensador@users.noreply.github.com>
2024-10-16 14:17:47 -0300

    Feat: Adiciona Cards de Estados ao CRM-Service

--- src/app/services/crm/crm.service.ts
+++ src/app/services/crm/crm.service.ts
@@ -9,20 +9,43 @@ export class CRMService {
     return [
       {
         name: 'Ceará',
+        description:
+          'CONSELHO REGIONAL DE MEDICINA DO ESTADO DO CEARÁ (CREMEC)',
         link: 'https://portalservicos.cfm.org.br/portal/login/pessoa-juridica/CE',
+      },
+
+      {
+        name: 'Minas Gerais',
+        description:
+          'CONSELHO REGIONAL DE MEDICINA DO ESTADO DE MINAS GERAIS (CREMIMG)',
+        link: 'https://portalservicos.cfm.org.br/portal/mg',
+      },
+
+      {
+        name: 'Rio de Janeiro',
+        description:
+          'CONSELHO REGIONAL DE MEDICINA DO ESTADO DO RIO DE JANEIRO (CREMERJ)',
+        link: 'https://www.cremerj.org.br/servicoempresas/',
+      },
+
+      {
+        name: 'São Paulo',
+        description:
+          'CONSELHO REGIONAL DE MEDICINA DO ESTADO DE SAO PAULO (CREMESP)',
+        link: 'https://www.cremesp.org.br/?siteAcao=ServicosEmpresas',
       },
 
       {
         name: 'lorem',
         description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloremque et distinctio deleniti nam, impedit voluptates.',
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores.',
         link: '',
       },
 
       {
         name: 'lorem',
         description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloremque et distinctio deleniti nam, impedit voluptates.',
+          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores.',
         link: '',
       },
     ];
