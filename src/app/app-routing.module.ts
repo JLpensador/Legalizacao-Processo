940ad196bf3a47aec6f11090d9f69b20ca478159
Emanuel Victor <emanuelvictor321321@gmail.com>
2024-10-16 15:31:56 -0300

    Feat: Adiciona Página Junta Comercial ao Projeto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,17 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './pages/home/home.component';
 import { AlvaraComponent } from './pages/alvara/alvara.component';
 import { CrmComponent } from './pages/crm/crm.component';
+import { JuntaComercialComponent } from './pages/junta-comercial/junta-comercial.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'alvará', component: AlvaraComponent },
   { path: 'CRM', component: CrmComponent },
+  { path: 'junta-comercial', component: JuntaComercialComponent}
 ];
 
 @NgModule({
