7a9b52aff69283d55b9f011e6c9cf98bddd1189a
João Luiz B. Morais <127153172+JLpensador@users.noreply.github.com>
2024-10-17 14:20:21 -0300

    Feat: Adiciona Cards de Alvarás dos Estados

--- src/app/services/alvará/alvarás.service.ts
+++ src/app/services/alvará/alvarás.service.ts
@@ -17,10 +17,36 @@ export class AlvarásService {
       },
 
       {
-        name: 'lorem',
+        name: 'Crato',
         description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloremque et distinctio deleniti nam, impedit voluptates.',
-        link: '',
+          'O site de serviços do Crato oferece uma plataforma para consultas e emissão de documentos públicos, como alvarás, certidões e licenças.',
+        link: 'https://servicos2.speedgov.com.br/crato/',
+        instruction: 'https://youtu.be/gBFucqMWNOQ?si=NkNOCbYukInUtSKd',
+      },
+
+      {
+        name: 'Eusébio',
+        description:
+          'O site de serviços do Eusébio proporciona uma interface prática para a solicitação e emissão de documentos públicos, como alvarás, certidões e licenças.',
+        link: 'https://servicos2.speedgov.com.br/eusebio/',
+        instruction: 'https://www.youtube.com/watch?v=gBFucqMWNOQ&t=1s',
+      },
+
+      {
+        name: 'Juazeiro do Norte',
+        description:
+          'O site de serviços de Juazeiro do Norte oferece uma plataforma eficiente para a emissão de documentos públicos, incluindo alvarás, certidões e licenças.',
+        link: 'https://servicos2.speedgov.com.br/juazeirodonorte/',
+        instruction: 'https://www.youtube.com/watch?v=SJn0oKUIu9I',
+      },
+
+      {
+        name: 'Rio de Janeiro',
+        description:
+          'O portal Carioca oferece uma solução digital inovadora para facilitar a vida dos cidadãos do Rio de Janeiro permitindo a emissão de alvarás, licenças e certidões.',
+        link: 'https://home.carioca.rio/',
+        instruction:
+          'https://services-carioca.rio.rj.gov.br/documents/10279/1093251601/faqnew/86d06a81-5c9c-5d4f-715b-a466691c5c57',
       },
 
       {
@@ -28,27 +54,7 @@ export class AlvarásService {
         description:
           'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloremque et distinctio deleniti nam, impedit voluptates.',
         link: '',
-      },
-
-      {
-        name: 'lorem',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloremque et distinctio deleniti nam, impedit voluptates.',
-        link: '',
-      },
-
-      {
-        name: 'lorem',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloremque et distinctio deleniti nam, impedit voluptates.',
-        link: '',
-      },
-
-      {
-        name: 'lorem',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores doloremque et distinctio deleniti nam, impedit voluptates.',
-        link: '',
+        instruction: '',
       },
     ];
   }
