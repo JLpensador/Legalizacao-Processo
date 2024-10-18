97f837e2334a5f1a120fafa096828c47353e083f
João Luiz B. Morais <127153172+JLpensador@users.noreply.github.com>
2024-10-18 15:21:20 -0300

    Feat Adiciona Tela de loading ao Projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'legalizacao-processos';
+
+  loader = true;
+  ngOnInit() {
+    setTimeout(() => {
+      this.loader = false;
+    }, 2000);
+  }
 }
